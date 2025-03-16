65ded1867a14078df800ad6749e7d4bab561411d
Aland Osman <aland.o@the-gw.com>
2025-03-16 22:13:54 +0300

    Revamp Gemini command

--- src/main/java/lunatix/ragscan/gemini/GeminiCommand.java
+++ src/main/java/lunatix/ragscan/gemini/GeminiCommand.java
@@ -1,62 +1,62 @@
 package lunatix.ragscan.gemini;
 
 import java.util.List;
-import java.util.Objects;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-import org.springframework.web.client.RestClient;
 
 @ShellComponent
 @RequiredArgsConstructor
 public class GeminiCommand {
 
-    private static final String GEMINI_URI = "/v1beta/models/gemini-1.5-flash-latest:generateContent?key={key}";
-
     private final VectorStore vectorStore;
-    private final RestClient restClient;
-
-    @Value("${spring.ai.vertex.ai.api-key}")
-    private String apiKey;
+    private final ChatModel chatModel;
 
     @ShellMethod(
             key = "ask",
             value = "Ask a question, Note: you need to load files",
             group = "Chat")
     private String ask(String question) {
-        final var resultInDB = vectorStore.similaritySearch(SearchRequest.query(question).withTopK(5));
-        final var generateContentRequest = getGenerateContentRequest(question, resultInDB);
-        return Objects.requireNonNull(restClient.post()
-                        .uri(GEMINI_URI, apiKey)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(generateContentRequest)
-                        .retrieve()
-                        .body(GeminiResponse.class))
-                .getCandidates().getFirst()
-                .getContent()
-                .getParts()
-                .getFirst()
-                .getText();
+        final var resultInDB = vectorStore.similaritySearch(
+                SearchRequest.builder()
+                        .query(question)
+                        .topK(5)
+                        .build()
+        );
+        assert resultInDB != null && !resultInDB.isEmpty()
+                : "error getting context";
+
+        final var generatedPrompt = getGenerateContentRequest(question, resultInDB)
+                .toArray(new Message[0]);
+        return chatModel.call(generatedPrompt);
     }
 
-    private static GenerateContentRequest getGenerateContentRequest(String message, List<Document> resultInDB) {
+    private static List<Message> getGenerateContentRequest(String message, List<Document> resultInDB) {
         final var systemContent = """
-                Answer only from the context, otherwise say you don't know, and clean up weird formats
+                Answer only from the data you got as input, otherwise say you don't know, and clean up weird formats
                 like if it's json clean it up, if it's Markdown clean it up etc...
-                context: %s
+                Your name is Ragscan.
+                You will always get some questions with some context. Use the context only.
+                """;
+        final var systemMessage = new SystemMessage(systemContent);
+        final var messageContent = """
+                Question: %s
+                Context: %s
                 """;
 
-        return new GenerateContentRequest(
-                List.of(new ContentRequest(List.of(new PartRequest(message)))),
-                new ContentRequest(List.of(
-                        new PartRequest(String.format(systemContent, resultInDB))))
+        final var userMessage = new UserMessage(String.format(messageContent, message, resultInDB.toString()));
+
+        return List.of(
+                systemMessage,
+                userMessage
         );
     }
 }
